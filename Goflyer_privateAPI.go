23215499c053c414acec90168f23131abf017312
applepine1125 <itn.siknh.bonvoyage@gmail.com>
2017-11-24 18:04:31 +0900

    :rocket: add GetPermission method and byteSlice2strSlice func

--- Goflyer_privateAPI.go
+++ Goflyer_privateAPI.go
@@ -1,8 +1,22 @@
 package Goflyer
 
-func (a *API) GetPermissions() ([]byte, error) {
+import "strings"
+
+func (a *API) GetPermissions() ([]string, error) {
 	path := "/v1/me/getpermissions"
 
 	byteSlice, err := a.PrivateAPIRequest(path, "GET", "")
-	return byteSlice, err
+	return byteSlice2strSlice(byteSlice), err
+}
+
+func byteSlice2strSlice(bs []byte) []string {
+
+	ss := strings.SplitAfter(string(bs), "\",\"")
+	for i, _ := range ss {
+		ss[i] = strings.TrimSuffix(ss[i], "\",\"")
+		ss[i] = strings.TrimPrefix(ss[i], "[\"")
+		ss[i] = strings.TrimSuffix(ss[i], "\"]")
+	}
+
+	return ss
 }
