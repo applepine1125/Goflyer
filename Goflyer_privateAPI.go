c9c12663b9dd4a331dd7758db11539e752aaaa56
applepine1125 <itn.siknh.bonvoyage@gmail.com>
2017-11-24 18:14:54 +0900

    :rocket: add GetBalance method and Balance struct

--- Goflyer_privateAPI.go
+++ Goflyer_privateAPI.go
@@ -1,6 +1,9 @@
 package Goflyer
 
-import "strings"
+import (
+	"encoding/json"
+	"strings"
+)
 
 func (a *API) GetPermissions() ([]string, error) {
 	path := "/v1/me/getpermissions"
@@ -10,8 +13,8 @@ func (a *API) GetPermissions() ([]string, error) {
 }
 
 func byteSlice2strSlice(bs []byte) []string {
-
 	ss := strings.SplitAfter(string(bs), "\",\"")
+
 	for i, _ := range ss {
 		ss[i] = strings.TrimSuffix(ss[i], "\",\"")
 		ss[i] = strings.TrimPrefix(ss[i], "[\"")
@@ -19,4 +22,25 @@ func byteSlice2strSlice(bs []byte) []string {
 	}
 
 	return ss
+}
+
+type Balance struct {
+	CurrencyCode string  `json:"currency_code"`
+	Amount       float64 `json:"amount"`
+	Available    float64 `json:"available"`
+}
+
+func (a *API) GetBalance() ([]Balance, error) {
+	path := "/v1/me/getbalance"
+
+	var balances []Balance
+	byteSlice, err := a.PrivateAPIRequest(path, "GET", "")
+
+	if err != nil {
+		return balances, err
+	}
+
+	err = json.Unmarshal(byteSlice, &balances)
+
+	return balances, err
 }
